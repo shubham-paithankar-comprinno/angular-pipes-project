39c84a176702b3c02fdbacc04b2c202d2c454ef8
RYUU <shubham.paithankar@comprinno.net>
2021-12-07 12:13:16 +0530

    added date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Variables
   name: string = ''
+  date: string = ''
+
+  // Functions
   onNameChange (event: Event) {
     let { value = '' } = event.target as HTMLInputElement
     this.name = value
+  }
+
+  onDateChange (event: Event) {
+    let { value = '' } = event.target as HTMLInputElement
+    this.date = value
   }
 }
